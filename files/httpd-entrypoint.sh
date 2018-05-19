47c54126d69d15ca36980861a00f350013bd74ce
François MICAUX <fmicaux_at_actilis_._net>
2018-05-20 00:18:06 +0200

    Add an Alpine Linux version

--- files/httpd-entrypoint.sh
+++ files/httpd-entrypoint.sh
@@ -1,8 +1,31 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # prefork / worker / event
 MPM=${HTTPD_MPM:=event}
-echo "LoadModule mpm_${MPM}_module modules/mod_mpm_${MPM}.so"  > /etc/httpd/conf.modules.d/00-mpm.conf
+
+# On RH/CentOS/Fedora
+if which rpm >/dev/null 2>/dev/null ; then
+	CONF_DIR=/etc/httpd/conf.d
+	echo "LoadModule mpm_${MPM}_module modules/mod_mpm_${MPM}.so"  > /etc/httpd/conf.modules.d/00-mpm.conf
+fi
+
+# Alpine
+if which apk &>/dev/null ; then
+	CONF_DIR=/etc/apache2/conf.d
+	sed -i -e '/LoadModule mpm_event/s/^#//' \
+	       -e '/LoadModule mpm_prefork/s/^/#/' \
+		/etc/apache2/httpd.conf
+
+	# Move files copied by Dockerfile in a RH/CentOS place to the right Alpine place
+	mv /etc/php-fpm.d/* /etc/php7/php-fpm.d && rm -rf /etc/php-fpm.d
+	mv /etc/httpd/conf.d/* /etc/apache2/conf.d/ && rm -rf /etc/httpd
+	# mod_proxy_fcgi uniquement
+	sed -i -e '/^LoadModule/s/^/#/' \
+               -e '/proxy_module/s/^#//' \
+               -e '/proxy_fcgi_module/s/^#//' \
+	        /etc/apache2/conf.d/proxy.conf
+	install -d -o apache -g apache -m 700 /var/log/httpd
+fi
 
 # Apache MPM Tuning defaults 
 export MPM_START=${MPM_START:-5}
@@ -13,5 +36,8 @@ export MPM_MAXCONNECTIONS=${MPM_MAXCONNECTIONS:-0}
 
 # TODO : Apply MPM conf in 00-mpm.conf to fit variables.
 
+# For PID-file
+mkdir -p /run/apache2
 
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+# Do it !
+exec "$@"
